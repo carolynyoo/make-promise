503683e3b7a3e590b07d3429ff3c02c84e115e7e
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-04-11 20:31:38 -0400

    All but last test spec in ch 2

--- pledge.js
+++ pledge.js
@@ -3,10 +3,29 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
-// Chapter 1
-
 var $Promise = function () {
   this.state = 'pending';
+  this.handlerGroups = [];
+};
+
+$Promise.prototype.then = function (successCb, errorCb) {
+  if (typeof successCb!=='function') {
+    successCb = null; 
+  }
+  if (typeof errorCb!=='function') {
+    errorCb = null;
+  }
+  this.handlerGroups.push({
+    successCb: successCb, 
+    errorCb: errorCb
+  });
+  if (this.state==='resolved') {
+    this.callHandlers();
+  }
+};
+
+$Promise.prototype.callHandlers = function () {
+  return this.handlerGroups.shift().successCb(this.value);
 };
 
 var Deferral = function () {
@@ -21,6 +40,10 @@ Deferral.prototype.resolve = function (data) {
   if (this.$promise.state === 'pending') {
     this.$promise.value = data;
     this.$promise.state = 'resolved';
+    // iffy 
+    for (var i=0; i<this.$promise.handlerGroups.length; i++) {
+      this.$promise.callHandlers();
+    }
   }
 };
 
@@ -30,10 +53,6 @@ Deferral.prototype.reject = function (err) {
     this.$promise.state = 'rejected';
   }
 };
-
-// Chapter 2
-
-
 
 /*-------------------------------------------------------
 The spec was designed to work with Test'Em, so we don't
